f8aa48262ea31ef3ec20a26cf746b44449d34379
devine1923@163.com <devine1923@163.com>
2020-04-09 16:54:28 +0800

    自定义简单的登录页面页面配置1.0

--- spring-security-browser/src/main/java/com/xh/security/config/BrowserSecurityConfig.java
+++ spring-security-browser/src/main/java/com/xh/security/config/BrowserSecurityConfig.java
@@ -1,5 +1,6 @@
-package com.xh.demo.config;
+package com.xh.security.config;
 
+import com.xh.security.consts.URLConst;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,9 +27,14 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
+                .loginPage(URLConst.SIMPLE_SIGNIN_PAGE_PATH)                        //自定义登录页面1.1
+                .loginProcessingUrl(URLConst.AUTHENTICATION_FORM_PATH)              //自定义处理登录的请求路径1.1
                 .and()
-                .authorizeRequests()        //授权请求
-                .anyRequest()               //拦截所有
-                .authenticated();           //需要认证
+                .authorizeRequests()        //授权请求1.0
+                .antMatchers(URLConst.SIMPLE_SIGNIN_PAGE_PATH).permitAll()          //指定放行路径1.1
+                .anyRequest()               //拦截所有1.0
+                .authenticated()           //需要认证1.0
+                .and()
+                .csrf().disable();          //关闭跨站伪造攻击的防护1.2
     }
 }
